4d3920a184a95d66cefc56f0721308952ea7398c
Alexey "LEHAtupointow" Pavlov <71947459+leha-code@users.noreply.github.com>
2022-02-19 17:27:43 -0400

    Revert "Apps testing"

--- scripts/VLC/red-hat-installer.sh
+++
@@ -1,7 +0,0 @@
-#vlc installer for red hat linux
-
-su -
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-yum install https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
-yum install vlc
-yum install vlc-core (for minimal headless/server install)
