f3a68343c11b5d56e7546ec24685c788daf54d2f
atwoyearold14 <97934952+atwoyearold14@users.noreply.github.com>
2022-02-19 16:24:40 -0800

    Delete scripts/VLC directory
    will re-upload updated files

--- scripts/VLC/gentoo-installer.sh
+++
@@ -1,6 +0,0 @@
-#installer for vlc on gentoo linux
-
-emerge --sync
-emerge --update --deep --with-bdeps y @world
-
-emerge vlc
--- scripts/VLC/opensuse-installer.sh
+++
@@ -1,7 +0,0 @@
-#installer for vlc on opensuse
-
-suzo zypper update
-
-sudo zypper ar https://download.videolan.org/SuSE/<SUSE version> VLC
-sudo zypper mr -r VLC
-sudo zypper in vlc
