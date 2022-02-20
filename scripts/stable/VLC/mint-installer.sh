286f0562346ead53b5fea6d1fb5a514e101cea09
leha-code <pezleha@gmail.com>
2022-02-19 20:51:39 -0400

    Cleanup after poopy branches

---
+++ scripts/stable/VLC/mint-installer.sh
@@ -0,0 +1,5 @@
+#installer for vlc on linux mint
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install vlc -y
---
+++ scripts/testing/Tuxemon/all-install.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/tuxemon/tuxemon
+shortcut tuxemon/run_tuxemon.py 
