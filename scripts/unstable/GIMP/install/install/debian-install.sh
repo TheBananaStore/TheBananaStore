b225aedcd9880101e9d9ebab75e55f72467c4796
atwoyearold14 <97934952+atwoyearold14@users.noreply.github.com>
2022-02-19 21:54:59 -0800

    Delete scripts/unstable/GIMP directory

--- scripts/unstable/GIMP/install/install/debian-install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# GIMP installer for Debain based GNU/Linux distros.
-
-sudo apt-get update
-
-sudo apt install gimp
--- scripts/unstable/GIMP/install/install/mint-install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# GIMP installer for Linux Mint.
-
-sudo add-apt-repository ppa:ubuntuhandbook1/gimp
-sudo apt update
-sudo apt install gimp
