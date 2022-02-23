249f544dda990732dcefe1fca5dca1e259787d3e
atwoyearold14 <97934952+atwoyearold14@users.noreply.github.com>
2022-02-23 07:52:47 -0800

    added -y flags, added update, and removed whitespace

--- scripts/unstable/Discord/debian-install.sh
+++ scripts/unstable/Discord/debian-install.sh
@@ -1,8 +1,10 @@
 # !/usr/bin/env bash
 # Discord installer for Debian.
 
+sudo apt-get update -y
+
 wget -O discord_latest.deb https://discord.com/api/download?platform=linux&format=deb
 
-sudo apt install ./discord.deb
+sudo apt install ./discord.deb -y
 
 rm discord.deb
