1f9d0a3b6138f6ef596853b77d2a383ab5def1fe
atwoyearold14 <97934952+atwoyearold14@users.noreply.github.com>
2022-02-22 20:08:12 -0800

    made sublime text install for fedora :nauseated_face:

---
+++ scripts/unstable/sublime-text/fedora-install.sh
@@ -0,0 +1,8 @@
+# !/usr/bin/env bash
+# Sublime Text for Fedora...
+
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
+sudo dnf install sublime-text
